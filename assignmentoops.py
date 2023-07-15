fa5fa0be412f1555f1d34e08c2713937f6206035
vaishali tiwari <136571044+maahi104@users.noreply.github.com>
2023-07-15 20:36:59 +0530

    Update assignmentoops.py

--- assignmentoops.py
+++ assignmentoops.py
@@ -4,8 +4,12 @@ class Point:
         self.y = y
         self.z = z
 
-    def sqSum(self):
+    def sum_of_squares(self):
         return self.x**2 + self.y**2 + self.z**2
+        
 
-p = Point(1, 3, 5)
-print(p.sqSum())
+x = int(input("Enter x-coordinate: "))
+y = int(input("Enter y-coordinate: "))
+z = int(input("Enter z-coordinate: "))
+object1 = Point(x, y, z)
+print("Sum of squared numbers:", object1.sum_of_squares())
